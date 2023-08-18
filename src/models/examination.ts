e37e3bac06b69ee924019adbd826aef742dc7c6b
lod <djumicbranislav@outlook.com>
2023-08-18 23:12:08 +0200

    Added data models

---
+++ src/models/examination.ts
@@ -0,0 +1,40 @@
+import { Schema, Model, model } from "mongoose";
+import { UserModel, IUser, IUserMethods, EUserRole } from "./user";
+
+export interface IExamination extends IUser {
+    specializations_id: IExamination;
+    name: string;
+    duration: number;
+    price: number;
+    disabled: boolean;
+}
+
+interface IExaminationMethods extends IUserMethods {}
+
+type TExaminationModel = Model<IExamination, {}, IExaminationMethods>;
+
+const managerSchema = new Schema<IExamination, TExaminationModel, IExaminationMethods>({
+    specializations_id: {
+        type: [Schema.Types.ObjectId],
+        ref: "Specialization",
+        required: [true, "specializations_id is required"],
+    },
+    name: {
+        type: String,
+        required: [true, "name is required"],
+    },
+    duration: {
+        type: Number,
+        default: 30,
+    },
+    price: {
+        type: Number,
+        required: [true, "price is required"],
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+export const ExaminationModel = model("Examination", managerSchema, "examinations");
---
+++ src/models/specialization.ts
@@ -0,0 +1,27 @@
+import { Schema, Model, model } from "mongoose";
+import { UserModel, IUser, IUserMethods, EUserRole } from "./user";
+import { ExaminationModel, IExamination } from "./examination";
+
+export interface ISpecialization extends IUser {
+    name: string;
+    examinations: IExamination[];
+}
+
+interface ISpecializationMethods extends IUserMethods {}
+
+type TSpecializationModel = Model<ISpecialization, {}, ISpecializationMethods>;
+
+const managerSchema = new Schema<ISpecialization, TSpecializationModel, ISpecializationMethods>({
+    name: {
+        type: String,
+        required: [true, "name is required"],
+    },
+    examinations: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Examination",
+        },
+    ],
+});
+
+export const SpecializationModel = model("Specialization", managerSchema, "specializations");
